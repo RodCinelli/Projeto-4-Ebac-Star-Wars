fd69d2dd2e611067654a561fb7a9f2a5b02562a3
Rodrigo Cinelli <rodcinelli@gmail.com>
2024-05-01 17:44:29 -0300

    Corrigindo problema do build

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
-
-function scripts() {
-    return gulp.src('./src/scripts/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist/js'));
-}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -14,7 +9,19 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = gulp.parallel(styles, scripts);
+function images() {
+    return gulp.src('./src/images/**/*', { encoding: false })
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+}
+
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
